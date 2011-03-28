d5c56c8277e798b57302134ccf48b6b9edf31e81
gershwinou <gershwinou@5ab75a6e-9881-2b17-355b-2e6ae80329f9>
2011-03-28 17:24:38 +0000

    git-svn-id: http://booness.googlecode.com/svn/trunk@33 5ab75a6e-9881-2b17-355b-2e6ae80329f9

---
+++ Booness/grails-app/routes/ParseMailRoute.groovy
@@ -0,0 +1,18 @@
+import org.codehaus.groovy.grails.commons.*
+import fr.booness.*
+
+class ParseMailRoute {
+    def configure = {
+        def config = ConfigurationHolder.config
+        String imap=config.camel.route.imap
+        //println imap
+
+        from(imap).process{
+            println it.in.body
+
+        }.to("bean:mailService?method=parseCompte")
+
+    }
+
+   
+}
