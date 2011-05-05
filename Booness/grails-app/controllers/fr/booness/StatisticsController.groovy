01e88781e5da24168123bd3645b343536b5e5fe5
gershwinou@gmail.com <gershwinou@gmail.com@5ab75a6e-9881-2b17-355b-2e6ae80329f9>
2011-05-05 07:54:04 +0000

    git-svn-id: http://booness.googlecode.com/svn/trunk@58 5ab75a6e-9881-2b17-355b-2e6ae80329f9

---
+++ Booness/grails-app/controllers/fr/booness/StatisticsController.groovy
@@ -0,0 +1,50 @@
+package fr.booness
+
+import grails.plugins.springsecurity.Secured
+
+class StatisticsController {
+
+    def index = { }
+	
+	@Secured(['ROLE_USER'])
+	private String createJSON(User user){
+		def json="["
+		boolean first=true
+		Log.findAllByUser(user,[max:100, order:'desc', sort:'startDate']).each{
+			if(first){
+				first=false
+				json+="{"
+			}
+			else {
+				json+=",{"
+			}
+
+			json+="title:\""+it.title+"\","
+			json+="start:'"+it.startDate+"',"
+			json+="end:'"+it.endDate+"',"
+			json+="allDay:"+it.allday+","
+			json+="url:\"log/show/"+it.id+"\","
+			json+="backgroundColor: 'green'"
+			json+="}"
+		}
+		Event.findAll("from Event as b where b.class like '%Event' order by startDate").each{
+			if(first){
+				first=false
+				json+="{"
+			}
+			else {
+				json+=",{"
+			}
+
+			json+="title:\""+it.title+"\","
+			json+="start:'"+it.startDate+"',"
+			json+="end:'"+it.endDate+"',"
+			json+="allDay:"+it.allday+","
+			json+="url:\"${controllerName}/show/"+it.id+"\","
+			json+="backgroundColor: 'blue'"
+			json+="}"
+		}
+		json+="]"
+		return json
+	}
+}
