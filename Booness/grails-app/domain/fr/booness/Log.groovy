e29b9fa7e4d00d813addbe77318a830950010281
gershwinou@gmail.com <gershwinou@gmail.com@5ab75a6e-9881-2b17-355b-2e6ae80329f9>
2011-06-04 13:44:13 +0000

    git-svn-id: http://booness.googlecode.com/svn/trunk@63 5ab75a6e-9881-2b17-355b-2e6ae80329f9

--- Booness/grails-app/domain/fr/booness/Log.groovy
+++ Booness/grails-app/domain/fr/booness/Log.groovy
@@ -5,11 +5,13 @@ class Log extends Event{
     Compte compte
     User user
 	String objectif
+	Integer kilometres = 0
 
     static belongsTo = [compte:Compte, user:User]
 
     static constraints = {
 		objectif(size:0..4096)
+		kilometres(nullable:true, min:0)
     }
 
 }
