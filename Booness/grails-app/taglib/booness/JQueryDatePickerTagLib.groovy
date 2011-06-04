b7571489425565afe81896b33880acfe5620955c
gershwinou@gmail.com <gershwinou@gmail.com@5ab75a6e-9881-2b17-355b-2e6ae80329f9>
2011-06-04 13:44:51 +0000

    git-svn-id: http://booness.googlecode.com/svn/trunk@64 5ab75a6e-9881-2b17-355b-2e6ae80329f9

--- Booness/grails-app/taglib/booness/JQueryDatePickerTagLib.groovy
+++ Booness/grails-app/taglib/booness/JQueryDatePickerTagLib.groovy
@@ -7,12 +7,12 @@ class JQueryDatePickerTagLib {
 		def value=attrs.value
 		def id= attrs.id?:name
 		
-		out.println "<input style=\"width=100px;\" type=\"text\" name=\"${name}\" id=\"${id}\" value=\"${attrs.value?.format('MM/dd/yyyy')}\" />"
-		out.println "<input type=\"hidden\" name=\"${name}_day\" id=\"${id}_day\" />"
-		out.println "<input type=\"hidden\" name=\"${name}_month\" id=\"${id}_month\" />"
-		out.println "<input type=\"hidden\" name=\"${name}_year\" id=\"${id}_year\" />"
+		out.println "<input style=\"width=100px;\" type=\"text\" name=\"${name}\" id=\"${id}\" value=\"${attrs.value?.format('dd/MM/yyyy')}\" />"
+		out.println "<input type=\"hidden\" name=\"${name}_day\" id=\"${id}_day\"  value=\"${attrs.value?.format('dd')}\" />"
+		out.println "<input type=\"hidden\" name=\"${name}_month\" id=\"${id}_month\"  value=\"${attrs.value?.format('MM')}\" />"
+		out.println "<input type=\"hidden\" name=\"${name}_year\" id=\"${id}_year\"  value=\"${attrs.value?.format('yyyy')}\" />"
 		out.println "<select style=\"width=30px;\" type=\"select\" name=\"${name}_hour\" id=\"${id}_hour\">"
-		[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23].each {
+		(0..23).each {
 			out.println "<option value=\"${it}\"${value?.getHours()==it?' selected':''}>${it<10?'0':''}${it}</option>"
 		}
 		out.println "</select>:"
@@ -27,12 +27,31 @@ class JQueryDatePickerTagLib {
 		<script type="text/javascript">
 		\$(document).ready(function(){
 		   \$("#'''+name+'''").datepicker({
+		   	  dateFormat:"dd/mm/yy",
+		   	  closeText: 'Fermer',
+			prevText: '&#x3c;Préc',
+			nextText: 'Suiv&#x3e;',
+			currentText: 'Courant',
+			monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin',
+			'Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
+			monthNamesShort: ['Jan','Fév','Mar','Avr','Mai','Jun',
+			'Jul','Aoû','Sep','Oct','Nov','Déc'],
+			dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+			dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
+			dayNamesMin: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
+			weekHeader: 'Sm',
+			dateFormat: 'dd/mm/yy',
+			firstDay: 1,
+			isRTL: false,
+			showMonthAfterYear: false,
+			yearSuffix: '',
 		   	  onClose: function(dateText, inst) {
-                 \$("#'''+name+'''_month").attr("value",new Date(dateText).getMonth() +1);
-                 \$("#'''+name+'''_day").attr("value",new Date(dateText).getDate());
-                 \$("#'''+name+'''_year").attr("value",new Date(dateText).getFullYear());
+                 \$("#'''+name+'''_month").attr("value",dateText.split("/")[1]);
+                 \$("#'''+name+'''_day").attr("value",dateText.split("/")[0]);
+                 \$("#'''+name+'''_year").attr("value",dateText.split("/")[2]);
               }
 		   });
+		   
 		})
 		</script>
 		'''
